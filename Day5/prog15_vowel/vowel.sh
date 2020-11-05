008ba5b79eed94849b99bb9474a133705341c4e8
dhanrz <dhanrz@pop-os.localdomain>
2020-11-05 15:07:59 +0530

    added capital letters

--- Day5/prog15_vowel/vowel.sh
+++ Day5/prog15_vowel/vowel.sh
@@ -7,5 +7,10 @@ case $ch in
 "i") echo "It is a vowel.";;
 "o") echo "It is a vowel.";;
 "u") echo "It is a vowel.";;
+"A") echo "It is a vowel.";;
+"E") echo "It is a vowel.";;
+"I") echo "It is a vowel.";;
+"O") echo "It is a vowel.";;
+"U") echo "It is a vowel.";;
 *) echo "It is not a vowel."
 esac
